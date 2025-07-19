afeca557d176fecca04b3d5c2f26ec63a884d8b4
Jason <jsofokleous@googlemail.com>
2025-07-19 13:53:45 +0100

    can now reset and record another video

--- src/App.tsx
+++ src/App.tsx
@@ -12,42 +12,48 @@ function App() {
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
   const blobChunksRef = useRef<Blob[]>([]);
 
-  useEffect(() => {
-    const setupRecorder = async () => {
-      if(!mediaRecorderRef.current) {
-        const {mediaRecorder, stream} = await getMediaRecorder();
-        if(!mediaRecorder || !stream) return;
-    
-        mediaRecorderRef.current = mediaRecorder;
-        setMediaStream(stream);
-
-        mediaRecorderRef.current.ondataavailable = (e) => {
-          if(e.data && e.data.size > 0) blobChunksRef.current.push(e.data);
-        };
+  const setupRecorder = async () => {
+    if(!mediaRecorderRef.current) {
+      const {mediaRecorder, stream} = await getMediaRecorder();
+      if(!mediaRecorder || !stream) return;
   
-        mediaRecorderRef.current.onstop = () => {
-          const blob = new Blob(blobChunksRef.current);
-          const file = new File([blob], "file1.webm", { type: "video/webm" });
-          setRecordedFile(file);
-        };
-      }
-    }
+      mediaRecorderRef.current = mediaRecorder;
+      setMediaStream(stream);
 
+      mediaRecorderRef.current.ondataavailable = (e) => {
+        if(e.data && e.data.size > 0) blobChunksRef.current.push(e.data);
+      };
+
+      mediaRecorderRef.current.onstop = () => {
+        const blob = new Blob(blobChunksRef.current);
+        const file = new File([blob], "file1.webm", { type: "video/webm" });
+        setRecordedFile(file);
+      };
+    }
+  }
+
+  useEffect(() => {
     setupRecorder();
   }, []);
 
   const toggleRecording = async () => {
-    if(!mediaRecorderRef.current) return
-
-    if(mediaRecorderRef.current.state == "recording") {
+    if(mediaRecorderRef.current?.state == "recording") {
       mediaRecorderRef.current.stop();
       mediaRecorderRef.current.stream.getTracks().forEach(track => track.stop());
       mediaRecorderRef.current = null;
       setMediaRecorderState("inactive");
-    } else {
-      mediaRecorderRef.current.start();
-      setMediaRecorderState("recording");
+      return;
     }
+
+    if(recordedFile) {
+      setRecordedFile(null);
+      setMediaStream(null);
+      blobChunksRef.current = [];
+    }
+
+    await setupRecorder();
+    mediaRecorderRef.current?.start();
+    setMediaRecorderState("recording");
   };
 
   return (
